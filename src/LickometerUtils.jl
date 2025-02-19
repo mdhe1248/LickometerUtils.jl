8be1e7f6183473985be268827968ac001ff181ee
Donghoon <mdhe1248@gmail.com>
2025-02-19 12:49:22 -0500

    export get_sampling_interval function, retire get_waittime.

--- src/LickometerUtils.jl
+++ src/LickometerUtils.jl
@@ -1,7 +1,7 @@
 module LickometerUtils
 
 export TouchSensor
-export get_recording_time, get_waittime, detect_touch, remove_long_touch, detect_lick, detect_touchmoment, estimate_baseline
+export get_recording_time, get_sampling_interval, detect_touch, remove_long_touch, detect_lick, detect_touchmoment, estimate_baseline
 
 include("lickometer.jl")
 # Write your package code here.
